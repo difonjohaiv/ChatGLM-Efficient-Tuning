e3db3985862dea22db8098b40b1ab709a5aa9423
snailshaw <snailshaw@126.com>
2023-07-21 22:45:04 +0800

    0

--- examples/fuse_finetuned.sh
+++ examples/fuse_finetuned.sh
@@ -2,9 +2,17 @@
 # coding=utf-8
 # a shell used to Exports the fine-tuned ChatGLM-6B model.
 
+# python ../src/export_model.py \
+#     --checkpoint_dir ..\\sft-ckp \
+#     --model_name_or_path ..\\glm2-6b \
+#     --dataset_dir ..\\data \
+#     --output_dir ..\\lora-glm2 \
+#     --use_v2
+
 python ../src/export_model.py \
-    --checkpoint_dir ..\\sft-ckp \
     --model_name_or_path ..\\glm2-6b \
+    --finetuning_type lora \
     --dataset_dir ..\\data \
+    --checkpoint_dir ..\\sft-ckp \
     --output_dir ..\\lora-glm2 \
     --use_v2
